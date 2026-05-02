e06a8e8b70a028e1c818defc54937266bd1a7d51
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-05-03 00:25:21 +0200

    create endpoints to get and add books

---
+++ backend/src/controllers/books.controller.ts
@@ -0,0 +1,36 @@
+import { Request, Response } from 'express'
+import pool from '../db'
+
+type Book = {
+	title: string
+	author: string
+}
+
+export const getBooks = async (req: Request, res: Response) => {
+	try {
+		const result = await pool.query('SELECT * FROM books')
+		return res.status(200).json(result.rows)
+	} catch (err) {
+		console.error(err)
+		return res.status(500).json({ error: 'Database error.' })
+	}
+}
+
+export const newBook = async (req: Request<{}, {}, Book>, res: Response) => {
+	const { title, author } = req.body
+	if (!title || !author || !title.trim() || !author.trim()) {
+		return res.status(400).json({ error: 'Missing data.' })
+	}
+	const titleFormatted = title.trim()
+	const authorFormatted = author.trim()
+	try {
+		const result = await pool.query('INSERT INTO books (title, author) VALUES ($1, $2) RETURNING *', [
+			titleFormatted,
+			authorFormatted,
+		])
+		return res.status(201).json({ message: 'New book registered successfully.', book: result.rows[0] })
+	} catch (err) {
+		console.error(err)
+		return res.status(500).json({ error: 'Database error.' })
+	}
+}
