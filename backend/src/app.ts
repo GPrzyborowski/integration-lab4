3db6438d60d01110572fcbb9a2ab29ed1e3d454e
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-05-03 00:26:56 +0200

    add books and loans endpoints

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,14 @@
 import express from 'express'
 import cors from 'cors'
 import pool from './db'
+import loans from './routes/loans.routes'
+import books from './routes/books.routes'
 
 const PORT = 5000
 const app = express()
 
 app.use(cors())
+app.use(express.json())
 
 app.use("/health", (req, res) => {
     res.send("ok")
@@ -15,6 +18,9 @@ app.use("/ready", async (req, res) => {
     const data = await pool.query("SELECT * FROM books")
     res.send(data.rows)
 })
+
+app.use("/api", loans)
+app.use("/api", books)
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
