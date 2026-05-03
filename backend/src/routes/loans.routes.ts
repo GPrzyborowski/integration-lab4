d5b34f4f3e4556bfff0f333827846889973a5fe5
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-05-03 05:25:23 +0200

    fix get loans route callback function

--- backend/src/routes/loans.routes.ts
+++ backend/src/routes/loans.routes.ts
@@ -1,9 +1,9 @@
 import express from 'express'
-import { newLoan } from '../controllers/loans.controller'
+import { getLoans, newLoan } from '../controllers/loans.controller'
 
 const router = express.Router()
 
-router.get("/get-loans", newLoan)
+router.get("/get-loans", getLoans)
 router.post("/new-loan", newLoan)
 
 export default router
