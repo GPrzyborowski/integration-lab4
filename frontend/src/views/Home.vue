b18a90b4b512d7d3f6cf987a038d132e47a30dff
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-05-03 04:22:30 +0200

    change buttons style

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -6,20 +6,18 @@ import Header from '../components/Header.vue'
 	<Header text="Simplified library management system" />
 	<img src="/library.png" alt="shelf with books" class="max-w-sm md:max-w-md lg:max-w-2xl mx-auto mt-12" />
 	<div class="flex flex-1 items-center justify-center mt-24">
-		<div class="flex">
-			<router-link to="/books" class="group relative inline-block text-sm font-medium mx-4 text-indigo-600">
-				<span
-					class="absolute inset-0 translate-x-0.5 translate-y-0.5 bg-indigo-600 transition-transform group-hover:translate-x-0 group-hover:translate-y-0"></span>
+		<div class="flex gap-8 justify-center">
+			<router-link
+				to="/books"
+				class="rounded-lg border border-indigo-600 bg-indigo-600 px-12 py-3 text-white text-center transition-colors duration-200 hover:bg-transparent hover:text-indigo-600">
+				Books
+			</router-link>
 
-				<span class="relative block border border-current bg-white px-8 py-3"> Books </span></router-link
-			>
-
-			<router-link to="/loans" class="group relative inline-block text-sm font-medium mx-4 text-indigo-600">
-				<span
-					class="absolute inset-0 translate-x-0.5 translate-y-0.5 bg-indigo-600 transition-transform group-hover:translate-x-0 group-hover:translate-y-0"></span>
-
-				<span class="relative block border border-current bg-white px-8 py-3"> Loans </span></router-link
-			>
+			<router-link
+				to="/loans"
+				class="rounded-lg border border-indigo-600 bg-indigo-600 px-12 py-3 text-white text-center transition-colors duration-200 hover:bg-transparent hover:text-indigo-600">
+				Loans
+			</router-link>
 		</div>
 	</div>
 </template>
