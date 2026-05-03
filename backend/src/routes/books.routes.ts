35a06443103a037f206b5ed37e1de4e46e9ec24a
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-05-03 06:57:06 +0200

    use delete book endpoint

--- backend/src/routes/books.routes.ts
+++ backend/src/routes/books.routes.ts
@@ -1,9 +1,10 @@
 import express from 'express'
-import { getBooks, newBook } from '../controllers/books.controller'
+import { getBooks, newBook, deleteBook } from '../controllers/books.controller'
 
 const router = express()
 
 router.get("/get-books", getBooks)
 router.post("/new-book", newBook)
+router.delete("/delete-book/:id", deleteBook)
 
 export default router
