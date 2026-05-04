bbb3fea27424c742c9cf42817807c39191bf7b23
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-05-04 23:40:31 +0200

    replace book icon with book id

--- frontend/src/components/Book.vue
+++ frontend/src/components/Book.vue
@@ -15,11 +15,11 @@ const props = defineProps<Props>()
 			<button class="absolute right-4 w-5 cursor-pointer" @click="props.deleteHandler(props.id)">
 				<img src="/bin.svg" alt="trash icon" />
 			</button>
-			<span class="inline-block rounded-sm bg-indigo-600 p-2 text-white">
-				<img src="/book.svg" alt="book icon" />
+			<span class="inline-block rounded-sm bg-indigo-600 py-2 px-4 text-white">
+				<p>ID: {{ id }}</p>
 			</span>
 
-			<h3 class="mt-0.5 text-lg font-medium text-gray-900">{{ props.title }}</h3>
+			<h3 class="mt-2 text-lg font-medium text-gray-900">{{ props.title }}</h3>
 
 			<p class="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">{{ props.author }}</p>
 		</article>
