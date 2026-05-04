f157b1df95d2b3e52aec8aedfd5be60ea99d4bc3
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-05-04 23:39:36 +0200

    implement return loan function

--- backend/src/controllers/loans.controller.ts
+++ backend/src/controllers/loans.controller.ts
@@ -3,13 +3,15 @@ import pool from '../db'
 
 type Loan = {
 	bookId: number
-	borrowerName: string
+	borrowerId: number
 	date: string
 }
 
 export const getLoans = async (req: Request, res: Response) => {
 	try {
-		const result = await pool.query('SELECT * FROM loans')
+		const result = await pool.query(
+			'SELECT l.id AS id, b.title, u.name, l.loan_date, l.return_date FROM loans l JOIN books b ON l.book_id = b.id JOIN users u ON l.user_id = u.id ORDER BY l.id DESC',
+		)
 		return res.status(200).json(result.rows)
 	} catch (err) {
 		console.error(err)
@@ -17,16 +19,33 @@ export const getLoans = async (req: Request, res: Response) => {
 	}
 }
 
+export const returnLoan = async (req: Request<{ id: string }, {}, {}>, res: Response) => {
+	const { id } = req.params
+	if (!id) {
+		return res.status(400).json({ message: 'Missing data.' })
+	}
+	const idParsed = Number(id)
+	try {
+		await pool.query('UPDATE loans SET return_date = $1 WHERE id = $2 RETURNING *', [new Date(), idParsed])
+		return res.status(200).json({ message: 'Loan ended successfully.' })
+	} catch (err) {
+		console.error(err)
+		return res.status(500).json({ message: 'Database error.' })
+	}
+}
+
 export const newLoan = async (req: Request<{}, {}, Loan>, res: Response) => {
-	const { bookId, borrowerName, date } = req.body
-	if (!bookId || !borrowerName || !date) {
+	const { bookId, borrowerId } = req.body
+	if (!bookId || !borrowerId) {
 		return res.status(400).json({ message: 'Missing data.' })
 	}
 	try {
-		const result = await pool.query(
-			'INSERT INTO loans (book_id, borrower_name, loan_date) VALUES ($1, $2, $3) RETURNING *',
-			[bookId, borrowerName, date],
-		)
+		const borrowerName = await pool.query('SELECT name FROM users WHERE id=$1', [borrowerId])
+		const result = await pool.query('INSERT INTO loans (book_id, user_id, loan_date) VALUES ($1, $2, $3) RETURNING *', [
+			bookId,
+			borrowerId,
+			new Date(),
+		])
 		return res.status(201).json({ message: 'New loan registered successfully.', loan: result.rows[0] })
 	} catch (err) {
 		console.error(err)
