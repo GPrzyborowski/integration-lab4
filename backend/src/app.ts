6eaf3b85c622e48116608f1b562aa6ea5775ae2d
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-05-06 18:46:20 +0200

    add users routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,6 +3,7 @@ import cors from 'cors'
 import pool from './db'
 import loans from './routes/loans.routes'
 import books from './routes/books.routes'
+import users from './routes/users.routes'
 
 const PORT = 5000
 const app = express()
@@ -10,18 +11,19 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.use("/health", (req, res) => {
-    res.send("ok")
+app.use('/health', (req, res) => {
+	res.send('ok')
 })
 
-app.use("/ready", async (req, res) => {
-    const data = await pool.query("SELECT * FROM books")
-    res.send(data.rows)
+app.use('/ready', async (req, res) => {
+	const data = await pool.query('SELECT * FROM books')
+	res.send(data.rows)
 })
 
-app.use("/api", loans)
-app.use("/api", books)
+app.use('/api', loans)
+app.use('/api', books)
+app.use('/api', users)
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+	console.log(`Server running on port ${PORT}`)
 })
